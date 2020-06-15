8ea05116e14da2b72036aafa5200effead9857b5
Ömer Özak <ozak@users.noreply.github.com>
2020-06-15 18:00:17 -0500

    removed link Azure

--- workscript.sh
+++ workscript.sh
@@ -38,11 +38,11 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "link Azure"
+git commit -m "removed link Azure"
 git push -u origin master
 cd ..
 git add .
-git commit -m "link Azure"
+git commit -m "removed link Azure"
 git push -u origin master
 
 cd output
