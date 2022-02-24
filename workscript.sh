66f9f418c7e77f8edd96a534f5e613aa4b18f9ad
Ömer Özak <ozak@users.noreply.github.com>
2022-02-24 09:00:25 -0600

    updated homebrew instructions

--- workscript.sh
+++ workscript.sh
@@ -39,11 +39,11 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "updated theme"
+git commit -m "updated homebrew instructions"
 git push -u origin master
 cd ..
 git add .
-git commit -m "updated theme"
+git commit -m "updated homebrew instructions"
 git push -u origin master
 
 cd output
